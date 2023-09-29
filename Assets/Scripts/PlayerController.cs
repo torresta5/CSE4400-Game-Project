d118fe90c7e8c749ae640856c654bbd7f6c16d1a
torresta5 <torresta5@gmail.com>
2023-09-29 01:31:07 -0700

    Attached Player Controller Script to Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(0, 14f, 0);
+            
+        }
         
     }
 }
