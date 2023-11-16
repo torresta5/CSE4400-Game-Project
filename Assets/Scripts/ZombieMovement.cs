ed2daa4c96ea7e729b574ed893309953176a9430
torresta5 <torresta5@gmail.com>
2023-11-16 02:26:32 -0800

    Added enemy movement
    Enemy movement is working partially it will walk forward but stop at the patrol point.

--- Assets/Scripts/ZombieMovement.cs
+++ Assets/Scripts/ZombieMovement.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class ZombieMovement : MonoBehaviour
 {
-    public Transform[] patrolPoints; 
+    public Transform[] patrolPoints;
+    public float moveSpeed;
+    public int patrolDestination;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (patrolDestination == 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[0].position) < .5f)
+            {
+                patrolDestination = 1;
+            }
+        }
+
+        if (patrolDestination == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[0].position) < .5f)
+            {
+                patrolDestination = 0;
+            }
+        }
+
     }
 }
