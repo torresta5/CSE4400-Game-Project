4cba2d090a83fc02a6236d4e1baae7e73a0d2759
torresta5 <torresta5@gmail.com>
2023-11-29 20:31:41 -0800

    Deleted Get Component script
    Instead of using multiple colliders the player only has one box collider due to having issues when colliding with an enemy. This prevents the player from losing two hearts of health instead of one.

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -9,7 +9,7 @@ public class EnemyDamage : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Hitbox")
+        if(collision.gameObject.tag == "Player")
         {
             playerController.KBCounter = playerController.KBTotalTime;
             if(collision.transform.position.x <= transform.position.x)
