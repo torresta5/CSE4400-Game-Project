c9110f7382cd0c734d6190d804811629b921b06d
FRodriguez292 <145165292+FRodriguez292@users.noreply.github.com>
2023-12-12 16:51:12 -0800

    Melee Hunter
    Melee Hunter patrols and dashes at the player when in attack Range

--- Assets/Scripts/MeleeHunterMovement.cs
+++ Assets/Scripts/MeleeHunterMovement.cs
@@ -2,94 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RangedHunterMovement : MonoBehaviour
-{
-    [SerializeField] private GameObject player;
-    [SerializeField] private GameObject hunterGun;
-    [SerializeField] private GameObject bullet;
-
-    [SerializeField] Transform bulletSpawn;
-    [SerializeField] float patrolRange = 10f;
-    [SerializeField] float attackRange = 20f;
-    [SerializeField] private float moveSpeed = 5;
-    [SerializeField] private bool flip = true;
+public class MeleeHunterMovement : MonoBehaviour
+{ 
+    [SerializeField] private float attackRange = 20f;
+    [SerializeField] private float patrolRange = 10f;
+    [SerializeField] private bool flip;
+    private GameObject player;
+    
     private Rigidbody2D rgbd;
 
-    private Vector2 direction;
-
-    private float attackTime;
-    
     private float patrolPoint;
+    private float moveSpeed = 7.5f;
+    private float attackTime = 2f;
 
-    private bool canShoot = true;
     private bool patrol1 = true;
     private bool patrol2 = false;
+    private bool canAttack = false;    
 
     // Start is called before the first frame update
     void Start()
     {
         rgbd = GetComponent<Rigidbody2D>();
-        player = GameObject.FindGameObjectWithTag("Player");
         patrolPoint = transform.position.x;
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(PlayerInAttackRange())
+        if (PlayerInAttackRange()) 
         {
-            rgbd.velocity = new Vector2(0, rgbd.velocity.y);
             FaceThePlayer();
-            RotateWeapon();
-            Attack();
+            Attack();        
         }
-        else
+        else 
         {
             Patrol();
         }
     }
-
-    private void RotateWeapon()
+    
+    //returns true if the Player is in Attack Range
+    private bool PlayerInAttackRange()
     {
-        //direction = transform.position - player.transform.position;
-        direction = player.transform.position - transform.position;
-        hunterGun.transform.right = direction;
-        bulletSpawn.transform.right = direction;
-        if(transform.localScale.x < 0)
-        {
-            hunterGun.transform.right *= -1;
-            bulletSpawn.right *= -1;
-        }
-    }
-
-    private bool PlayerInAttackRange() 
-    {
-        return (Vector2.Distance(transform.position, player.transform.position) < attackRange) ;
-    }
-
-    private void Attack()
-    {
-        if (!canShoot)
-        {
-            attackTime += Time.deltaTime;
-            if (attackTime >= 3f)
-            {
-                attackTime = 0;
-                canShoot = true;
-            }
-        }
-        else
-        {
-            
-            Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
-            canShoot = false;
-        }
+        return (Vector2.Distance(transform.position, player.transform.position) <= attackRange);
     }
 
     private void Patrol()
     {
+        // makes sure the melee hunter is facing the correct direction
         if ((transform.localScale.x < 0 && patrol1) || (patrol2 && transform.localScale.x > 0))
         {
             Vector3 localScale = transform.localScale;
@@ -97,11 +57,11 @@ public class RangedHunterMovement : MonoBehaviour
             transform.localScale = localScale;
         }
 
-        if(patrol1)
+        if (patrol1)
         {
             rgbd.velocity = new Vector2(moveSpeed, rgbd.velocity.y);
 
-            if(transform.position.x > patrolPoint + patrolRange)
+            if (transform.position.x >= patrolPoint + patrolRange)
             {
                 Vector3 localScale = transform.localScale;
                 localScale.x *= -1;
@@ -111,7 +71,7 @@ public class RangedHunterMovement : MonoBehaviour
                 patrol2 = !patrol2;
             }
         }
-        else if(patrol2)
+        else if (patrol2)
         {
             rgbd.velocity = new Vector2(-moveSpeed, rgbd.velocity.y);
 
@@ -128,19 +88,40 @@ public class RangedHunterMovement : MonoBehaviour
         }
     }
 
-    private void FaceThePlayer() 
-    {     
+    // Function to make the Melee Hunter Face the Hunter as the name implies
+    private void FaceThePlayer()
+    {
         Vector3 scale = transform.localScale;
 
         if (player.transform.position.x > transform.position.x)
         {
             scale.x = Mathf.Abs(scale.x) * -1 * (flip ? -1 : 0);
-        } 
-        else 
+        }
+        else
         {
             scale.x = Mathf.Abs(scale.x) * (flip ? -1 : 1);
         }
 
-        transform.localScale = scale;  
+        transform.localScale = scale;
+    }
+
+    private void Attack() 
+    {
+        // moves to the left if the Melee Hunter is to the right of the player and vice versa
+        rgbd.velocity = new Vector2(moveSpeed * (transform.position.x >= player.transform.position.x ? -1: 1), rgbd.velocity.y);
+        if (!canAttack)
+        {
+            attackTime += Time.deltaTime;
+            if (attackTime >= 2f)
+            {
+                attackTime = 0;
+                canAttack = true;
+            }
+        }
+        else
+        {
+            rgbd.velocity = new Vector2(rgbd.velocity.x * 12.5f, rgbd.velocity.y);
+            canAttack = false;
+        }
     }
 }
