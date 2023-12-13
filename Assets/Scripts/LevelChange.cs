a55e0e6b8d21f31090f618ab5c7b023c448e07e7
torresta5 <torresta5@gmail.com>
2023-12-13 07:07:58 -0800

    Updated LevelChange.cs
    Updated the script so that it changes the values of the booleans for level1Complete and level2Complete in the StateNameController script.

--- Assets/Scripts/LevelChange.cs
+++ Assets/Scripts/LevelChange.cs
@@ -7,12 +7,21 @@ public class LevelChange : MonoBehaviour
 {
     public int sceneBuildIndex;
 
+    public int level;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
             SceneManager.LoadSceneAsync(sceneBuildIndex, LoadSceneMode.Single);
-            StateNameController.storyIndex += 1;
+            if(level == 1)
+            {
+                StateNameController.level1Complete = true;
+            }
+            else if (level == 2)
+            {
+                StateNameController.level2Complete = true;
+            }
         }
 
 
