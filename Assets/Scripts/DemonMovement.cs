48c804f2909232d6c73fd1f25a1678cc5ca2f101
torresta5 <torresta5@gmail.com>
2023-12-14 13:49:53 -0800

    push

--- Assets/Scripts/DemonMovement.cs
+++ Assets/Scripts/DemonMovement.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.EventSystems;
+//using UnityEditor.EventSystems;
 using UnityEngine;
 
 public class DemonMovement : MonoBehaviour
